2dbe67e3c680cd50fe5e795879d7ccf7dad6df56
alessandro teixeira <alessandroteixeira@mpf.mp.br>
2025-05-14 14:08:17 -0300

    provider token injection

---
+++ day05-02/src/app/app.config.ts
@@ -0,0 +1,16 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { customInterceptor } from './shared/interceptors/custom-interceptor';
+import { RAIZ_API } from './shared/providers/raiz-api';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(withInterceptors([customInterceptor])),
+    provideRouter(routes),
+    { provide: RAIZ_API, useValue: '/api' }
+  ]
+};
