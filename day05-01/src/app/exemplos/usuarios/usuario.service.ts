e1670cf0a3582140cb6e18d278610b0d0687fd1c
alessandro teixeira <alessandroteixeira@mpf.mp.br>
2025-05-14 12:54:04 -0300

    paginacao init

---
+++ day05-01/src/app/exemplos/usuarios/usuario.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Usuario } from '../../shared/model/usuario';
+import { catchError, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  private http = inject(HttpClient);
+
+  private urlApi = '/api/usuarios';
+
+  async carregarUsuariosPromise(abortSignal?: AbortSignal) {
+    try {
+      const res = await fetch(this.urlApi, { signal: abortSignal });
+
+      if (!res.ok)
+        throw new Error(`Não foi possível obter os dados. [Status: ${res.status}]`);
+
+      return await res.json() as Usuario[];
+    }
+    catch (error: any) {
+      if (error instanceof TypeError)
+        throw Error(`O destino ${this.urlApi} não pode ser alcançado. Verifique sua conexão.`);
+
+      throw error;
+    }
+  }
+
+  carregarUsuariosObservable() {
+    return this.http.get<Usuario[]>(this.urlApi)
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+          return throwError(() => Error(`Não foi possível obter os dados. [Status: ${error.status}]`));
+        })
+      );
+  }
+
+
+}
